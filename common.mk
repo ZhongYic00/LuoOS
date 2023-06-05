d49485bc69ef6addaa54fb9dc5729523a7cd159f
Ct_Unvs <1027238924@qq.com>
2023-06-05 17:23:05 +0000

    bug find in -smp

--- common.mk
+++ common.mk
@@ -4,7 +4,7 @@ CROSS_COMPILE = riscv64-unknown-elf-
 CFLAGS = -nostdlib -fno-exceptions -fno-builtin -march=rv64ima -mabi=lp64 -mcmodel=medany -Wall
 
 QEMU = qemu-system-riscv64
-QFLAGS =  -smp 1 -m 128M -machine virt -bios default -d int,mmu -nographic -drive file=fat32.img,if=none,format=raw,id=x0 -device virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0
+QFLAGS =  -smp 2 -m 128M -machine virt -bios default -d int,mmu -nographic -drive file=sdcard.img,if=none,format=raw,id=x0 -device virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0
 
 GDB = gdb-multiarch
 CC = ${CROSS_COMPILE}g++
