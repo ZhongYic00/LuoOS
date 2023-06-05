38f6a06cac30224745a0f2ef2225fe40b7caf828
Ct_Unvs <1027238924@qq.com>
2023-06-05 12:56:58 +0000

    implement getTimeOfDay()

--- kernel/syscall.cc
+++ kernel/syscall.cc
@@ -468,9 +468,7 @@ namespace syscall
         xlen_t ticks;
         asm volatile("rdtime %0" : "=r" (ticks) ); // todo@ 优化
         // todo@ timer相关
-        // #define CLK_FREQ 8900000
-        // TimeSpec ts(ticks/CLK_FREQ, ((ticks/(CLK_FREQ/100000))%100000)*10);
-        TimeSpec ts;
+        TimeSpec ts(ticks/CLK_FREQ, ((ticks/(CLK_FREQ/100000))%100000)*10);
         curproc->vmar.copyout((xlen_t)a_ts, klib::ByteArray((uint8_t*)&ts, sizeof(ts)));
 
         return statcode::ok;
