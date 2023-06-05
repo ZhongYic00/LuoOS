ac31164bb8bf6a93fcbc33c6eaef9f459d3f26c6
Ct_Unvs <1027238924@qq.com>
2023-06-05 09:26:03 +0000

    cant run through testsuits

--- user/init.cc
+++ user/init.cc
@@ -8,49 +8,49 @@ int main(){
 	    char **argv = reinterpret_cast<char**>((void *)(p+1));
     }
     sys::syscall(sys::syscalls::testfatinit);
-    char path[]="clone";
-    char *execargv[]={ nullptr };
-    if(sys::syscall(sys::syscalls::clone)){
-        sys::syscall2(sys::syscalls::execve,(xlen_t)path,(xlen_t)execargv);
-    }
-    sys::syscall(sys::syscalls::exit);
+    // char path[]="clone";
+    // char *execargv[]={ nullptr };
+    // if(sys::syscall(sys::syscalls::clone)){
+    //     sys::syscall2(sys::syscalls::execve,(xlen_t)path,(xlen_t)execargv);
+    // }
+    // sys::syscall(sys::syscalls::exit);
     // while(true){
     //     sys::syscall2(sys::syscalls::wait,-1,0);
     // }
     // 测例代码
     char *testsuits[] = {
-        // "brk",
-        // "chdir",
-        // // "clone",
-        // "close",
-        // "dup",
-        // "dup2",
-        // // "execve", 失败
-        // "exit",
-        // "fork",
-        // "fstat",
-        // "getcwd",
-        // "getdents",
-        // "getpid",
-        // // "getppid",
-        // "gettimeofday",
-        // "mkdir_",
-        // // "mmap",
-        // "mount",
-        // // "munmap",
-        // "open",
-        // "openat",
-        // // "pipe",
-        // "read",
+        "brk",
+        "chdir",
+        // "clone",
+        "close",
+        "dup",
+        "dup2",
+        "execve",
+        "exit",
+        "fork",
+        "fstat",
+        "getcwd",
+        "getdents",
+        "getpid",
+        // "getppid",
+        "gettimeofday",
+        "mkdir_",
+        // "mmap",
+        "mount",
+        // "munmap",
+        "open",
+        "openat",
+        // "pipe",
+        "read",
         // "sleep",
-        // "times",
-        // "umount",
-        // "uname",
-        // "unlink",
-        // "wait",
-        "waitpid",
-        // "write",
-        // "yield"
+        "times",
+        "umount",
+        "uname",
+        "unlink",
+        "wait",
+        // "waitpid",
+        "write",
+        "yield"
     };
     const int tsn = sizeof(testsuits) / sizeof(char const*);
     char *args[] = { nullptr };
@@ -63,6 +63,7 @@ int main(){
             sys::syscall2(sys::syscalls::wait,-1,0);
         }
 	}
+    sys::syscall3(sys::syscalls::reboot, LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_POWER_OFF);
 	while(true);
 	// exit(0);
 }
