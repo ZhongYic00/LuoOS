b3003fff47bd8b729e7f539338dd4cce6fe20ee5
zhongyic00 <zhong200234@outlook.com>
2023-06-27 20:58:35 +0800

    feat: impl spinlock

--- include/lock.hh
+++ include/lock.hh
@@ -18,5 +18,45 @@ namespace semaphore
     };
 } // namespace semaphore
 
+namespace mutex
+{
+    template<typename count_t=xlen_t,xlen_t maxloops=std::numeric_limits<count_t>::max(),typename atomic_base_t=uint32_t>
+    class spinlock{
+        std::atomic<atomic_base_t> spin;
+    public:
+        bool lock(){
+            /// @todo optimize, use normal read to reduce buslock overhead
+again:      atomic_base_t expected=0;
+            bool b=true;
+            for(xlen_t n=maxloops;
+                b && n>0;
+                    n--){
+                        expected=0;
+                        b=!spin.compare_exchange_strong(expected,1);
+                    }
+            if(!b)return true;
+            if (maxloops==std::numeric_limits<count_t>::max())
+                goto again;
+            else return false;
+            // giveup
+        }
+        void unlock(){spin.store(0);}
+    };
+
+    class mutex{
+    };
+
+    template<typename lock_t>
+    class lock_guard{
+        lock_t& lock;
+    public:
+        explicit lock_guard(lock_t& lock_):lock(lock_){lock.lock();}
+        ~lock_guard(){lock.unlock();}
+
+        lock_guard(const lock_guard&) = delete;
+        lock_guard& operator=(const lock_guard&) = delete;
+    };
+} // namespace mutex
+
 
 #endif
