93a8d2b027bfa10a543b182bcb8226b4748fac9c
zhongyic00 <zhong200234@outlook.com>
2023-07-06 14:38:10 +0800

    feat: impl lockless ringbuf; feat: temp lock for puts

--- utils/klib.cc
+++ utils/klib.cc
@@ -1,12 +1,19 @@
 
 #include "klib.hh"
 #include "sbi.hh"
+#include "lock.hh"
 
 int enableLevel=LogLevel::info;
 
+mutex::spinlock<false> putslock;
 void IO::_sbiputs(const char *s){
+    mutex::lock_guard guard(putslock);
     size_t len=strlen(s);  
     sbi_debug_console_write(len,(xlen_t)s,0);
+}
+void IO::_blockingputs(const char *s){
+    mutex::lock_guard guard(putslock);
+    while(*s)platform::uart0::blocking::putc(*s++);
 }
 Logger kLogger;
 extern int _vsnprintf(char * out, size_t n, const char* s, va_list vl);
