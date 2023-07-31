7bed7108765ad376466658a92bda6f58625f3f8f
zhongyic00 <zhong200234@outlook.com>
2023-08-01 00:57:10 +0800

    fix: run time-test

--- user/init.cc
+++ user/init.cc
@@ -29,6 +29,7 @@ int main(){
     const char *args2[] = { "busybox","sh","-c","busybox grep -v -e tls_get -e sem_init -e pthread_cancel -e pthread_cond -e pthread_cond_ -e pthread_exit_cancel -e deadlock -e rwlock run-static.sh | while read line; do eval $line; done\n",nullptr };
     const char *args3[] = { "busybox","sh","-c","busybox grep -v -e tls_get -e sem_init -e pthread_cancel -e pthread_cond -e pthread_cond_ -e pthread_exit_cancel -e deadlock -e rwlock run-dynamic.sh  | while read line; do eval $line; done\n",nullptr };
     Exec testcases[]={
+        {"time-test",args0,envs},
         {"busybox",args0,envs},
         {"busybox",args1,envs},
         {"busybox",args2,envs},
