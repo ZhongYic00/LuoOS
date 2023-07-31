3c0627679a1454c0206d0dcaaf68d8abfadc4d1a
zhongyic00 <zhong200234@outlook.com>
2023-08-01 00:15:33 +0800

    temp: use blacklist to run tests

--- user/init.cc
+++ user/init.cc
@@ -1,5 +1,10 @@
 #include "kernel.hh"
 #include "linux/reboot.h"
+struct Exec{
+    const char *exec;
+    const char **args;
+    const char **envs;
+};
 int main(){
     register long *p asm("a0");
     // printf("init.elf\n");
@@ -18,47 +23,23 @@ int main(){
     //     sys::syscall2(sys::syscalls::wait,-1,0);
     // }
     // 测例代码
-    char *testsuits[] = {
-        // "brk",
-        // "chdir",
-        // "clone",
-        // "close",
-        // "dup",
-        // "dup2",
-        // "execve",
-        // "exit",
-        // "fork",
-        // "fstat",
-        // "getcwd",
-        // "getdents",
-        // "getpid",
-        // "getppid",
-        // "gettimeofday",
-        // "mkdir_",
-        // "mmap",
-        // "mount",
-        // "munmap",
-        // "open",
-        // "openat",
-        // "pipe",
-        // "read",
-        // "sleep",
-        // "times",
-        // "umount",
-        // "uname",
-        // "unlink",
-        // "wait",
-        // "waitpid",
-        // "write",
-        // "yield"
-        "./busybox"
+    const char *envs[] = {"PATH=/","LD_LIBRARY_PATH=/",nullptr};
+    const char *args0[]={"busybox","sh","lua_testcode.sh",nullptr};
+    const char *args1[]={"busybox","sh","busybox_testcode.sh",nullptr};
+    const char *args2[] = { "busybox","sh","-c","busybox grep -v -e tls_get -e sem_init -e pthread_cancel -e pthread_cond -e pthread_cond_ -e pthread_exit_cancel -e deadlock -e rwlock run-static.sh | while read line; do eval $line; done\n",nullptr };
+    const char *args3[] = { "busybox","sh","-c","busybox grep -v -e tls_get -e sem_init -e pthread_cancel -e pthread_cond -e pthread_cond_ -e pthread_exit_cancel -e deadlock -e rwlock run-dynamic.sh  | while read line; do eval $line; done\n",nullptr };
+    Exec testcases[]={
+        {"busybox",args0,envs},
+        {"busybox",args1,envs},
+        {"busybox",args2,envs},
+        {"busybox",args3,envs},
     };
-    const int tsn = sizeof(testsuits) / sizeof(char const*);
-    char *args[] = { "busybox","sh","run-static.sh",nullptr };
+    const int tsn = sizeof(testcases) / sizeof(Exec);
     // char *args[] = { "busybox","sh","run-static.sh",nullptr };
 	for (int i = 0; i < tsn; ++i) {
 		if (sys::syscall2(sys::syscalls::clone, 17, 0) == 0) {
-            sys::syscall2(sys::syscalls::execve, (xlen_t)testsuits[i], (xlen_t)args);
+            auto &test=testcases[i];
+            sys::syscall3(sys::syscalls::execve, (xlen_t)test.exec, (xlen_t)test.args,(xlen_t)test.envs);
             // exec(testsuits[i], (char**)0);
         }
 		else {
